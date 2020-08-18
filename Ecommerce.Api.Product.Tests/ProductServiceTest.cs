e6c59d56b5146b61a0013bf14987d09f55c5ba3d
Dilshan Manuranga Perera Makavitage <dper@win.dtu.dk>
2020-08-18 18:29:40 +0200

    containerized microservice

--- Ecommerce.Api.Product.Tests/ProductServiceTest.cs
+++ Ecommerce.Api.Product.Tests/ProductServiceTest.cs
@@ -12,6 +12,24 @@ namespace Ecommerce.Api.Product.Tests
 {
     public class ProductServiceTest
     {
+
+        private void CreateProduct(ProductDbContext dbContext)
+        {
+            for (int i = 10; i < 20; i++)
+            {
+                dbContext.Produts.Add(new ECommerce.Api.Products.DB.Product()
+                {
+                    Id = i,
+                    Name = Guid.NewGuid().ToString(),
+                    Inventory = i + 10,
+                    Price = (decimal)(i * 3.14)
+                });
+            }
+
+            dbContext.SaveChanges();
+        }
+
+
         [Fact]
         public async void GetProductsReturnAllProducts()
         {
@@ -32,22 +50,33 @@ namespace Ecommerce.Api.Product.Tests
             Assert.True(product.IsSucess);
             Assert.True(product.products.Any());
             Assert.Null(product.ErrorMessage);
+
+            dbContext.Produts = null;
         }
 
-        private void CreateProduct(ProductDbContext dbContext)
+
+        [Fact]
+        public async void GetProductsReturnOneProduct()
         {
-            for (int i = 10; i < 20; i++)
-            {
-                dbContext.Produts.Add(new ECommerce.Api.Products.DB.Product()
-                    {
-                        Id = i,
-                        Name = Guid.NewGuid().ToString(),
-                        Inventory = i+10,
-                        Price = (decimal)(i * 3.14)
-                    });
-            }
+            var options = new DbContextOptionsBuilder<ProductDbContext>().UseInMemoryDatabase("ProductDB").Options;
 
-            dbContext.SaveChanges();
+            var dbContext = new ProductDbContext(options);
+            CreateProduct(dbContext);
+
+
+            var productProfile = new ProductProfile();
+            var configuration = new MapperConfiguration(cfg => cfg.AddProfile(productProfile));
+            var mapper = new Mapper(configuration);
+
+            var productProvider = new ProductProvider(dbContext, null, mapper);
+
+            var product = await productProvider.GetProductAsync(10);
+
+            Assert.True(product.IsSucess);
+            Assert.NotNull(product.product);
+            Assert.Equal(10,product.product.Id);
+            Assert.Null(product.ErrorMessage);
         }
+
     }
 }
